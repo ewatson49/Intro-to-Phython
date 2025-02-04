09714477bfd9b2273484363ab45fcc9e70fbfada
ewatson49 <ewatson489@boyslatin.net>
2025-02-04 19:23:33 +0000

    first commit

---
+++ Unit 1/intro python.py
@@ -0,0 +1,31 @@
+# message = "hello from python"
+
+# the hashtag symbol is  a way to make 
+# anything invisible to a computer in python
+
+# text behind the hashtag is great for
+
+# Datatypes
+
+#"string datatype"
+# "a string is anything wrapped in quotes"
+
+# "we create strings by first clicking the quote button on the keyboard and then typing within the text."
+
+# "strings can contain any key on the keyboard."
+
+# "strings are used mostly for text based data like messages, comments, etc."
+
+# interger data type 
+
+# "just another way of saying number"
+
+# "don't need to wrap in quotes"
+# "when we have a number with a decimal it is considered a float data type" 
+
+# boolean data type 
+# a representation of true and false 
+False
+0 # represents false 
+True
+1 # represents true 
