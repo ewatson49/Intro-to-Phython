16fe9d523ae1ec7964032119283e36f33d11b190
ewatson49 <ewatson489@boyslatin.net>
2025-02-27 19:28:40 +0000

    learning functions

---
+++ Unit 1/asyncReviewFeb21.py/asyncReviewFeb21.py
@@ -0,0 +1,41 @@
+#1 
+'a python variable is a container for data types.'
+# name is the container and the value 'laj' is the data
+name = 'laj' 
+
+# 2 
+# what data type is inside of this variable?
+# this is a float data type. a float is a number with a decimal
+x = 102.3
+
+#3 
+# what operator would i use to divied two variables 
+'arithmatic operator'
+# use the print function to show the output of two variables 
+# being divided 
+
+numberOne = 8 
+numberTwo = 4
+
+print(numberOne / numberTwo)
+
+# 4 
+# what will happen if i pass in the string 49 in the int() funtion
+print (int('49')) # will transform string into interger
+
+
+#5 
+# what built in funtion would transform a interger into float 
+float(30)
+
+#6 
+creditsneededtopass = 100
+currentcredits = 80 
+
+100 > 80
+
+#7
+
+name = 'laj' 
+title = 'engineer'
+age = 'old' 
